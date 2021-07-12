87ef34ffe52061fcbb4f22fcd97764037717696a
Mich Talebzadeh <mich.talebzadeh@gmail.com>
2021-07-12 17:39:50 +0100

    some minor change

--- DSBQ/src/testme.py
+++ DSBQ/src/testme.py
@@ -1,24 +1,21 @@
 import sys
-from src.config import config, oracle_url
+from pyspark.sql import SparkSession
+from pyspark import SparkContext
+from pyspark.sql import SQLContext
+from pyspark.sql import HiveContext
 from pyspark.sql import functions as F
 from pyspark.sql.functions import col, round
-from pyspark.sql.window import Window
-from sparkutils import sparkstuff as s
-from othermisc import usedFunctions as uf
-import locale
-locale.setlocale(locale.LC_ALL, 'en_GB')
 
 def main():
     appName = "testme"
-    spark_session = s.spark_session(appName)
-    spark_context = s.sparkcontext()
+    spark_session = SparkSession.builder.enableHiveSupport().appName(appName).getOrCreate()
+    spark_context = SparkContext.getOrCreate()
     spark_context.setLogLevel("ERROR")
     print(spark_session)
     print(spark_context)
     rdd = spark_context.parallelize([1,2,3,4,5,6,7,8,9,10])
-    print(rdd)
-    rdd.collect()
-    print(f"""\n Printing a line from {appName}""")
+    print(rdd.collect())
+    print("\n Printing a line from testme")
 
 
 if __name__ == "__main__":
